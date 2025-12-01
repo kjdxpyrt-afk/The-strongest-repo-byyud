73e6e780ddca455656a5b30894ced5926603e99e
kjdxpyrt-afk <yudarajtimur@gmail.com>
2025-12-01 23:24:09 +0700

    Create build.gradle.kts

---
+++ src/id/komiku/build.gradle.kts
@@ -0,0 +1,11 @@
+apply(plugin = "tachiyomi.extension")
+
+extension {
+    name = "Komiku"
+    id = "komiku"
+    icon = "https://komiku.org/favicon.ico"
+    description = "Komiku Indonesia manga source"
+    versionCode = 1
+    lang = "id"
+    nsfw = false
+}
